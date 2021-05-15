adcfcdcdc6119bfaa013e9e573f6346712a1f9fd
disco07 <koneenok@outlook.fr>
2021-05-15 22:30:47 +0200

    petite modif

--- assets/components/left/Left.js
+++ assets/components/left/Left.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Left(props) {
+const Left = () => {
     return (
-        <>
-        </>
+        <div>
+
+        </div>
     );
-}
+};
 
 export default Left;
---
+++ assets/components/right/Blank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blank = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Blank;
