9cb109e82172cf287c37d88f8181e983f59535e8
disco07 <koneenok@outlook.fr>
2021-05-16 22:49:37 +0200

    petite modification de la partie chat du site

--- assets/components/right/Blank.js
+++ assets/components/right/Blank.js
@@ -15,6 +15,7 @@ const Blank = () => {
                                     <div className="col-md-12">
                                         <div className="no-messages">
                                             <i className="material-icons md-48">forum</i>
+                                            <p>Seems people are shy to start the chat. Break the ice send the first message.</p>
                                         </div>
                                     </div>
                                 </div>
