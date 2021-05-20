317708c631df50acc225b3244d23101f0ad570af
disco07 <koneenok@outlook.fr>
2021-05-20 23:57:33 +0200

    jwtToken

--- src/Events/JWTCreatedSubscriber.php
+++ src/Events/JWTCreatedSubscriber.php
@@ -11,8 +11,10 @@ class JWTCreatedSubscriber
     public function updateJwtData(JWTCreatedEvent $event) {
         $user = $event->getUser();
         $data = $event->getData();
+        $data["id"] = $user->getId();
         $data["firstName"] = $user->getFirstName();
         $data["lastName"] = $user->getLastName();
+        $data["avatar"] = $user->getAvatar();
 
         $event->setData($data);
     }
