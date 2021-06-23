cda890ebc31c0f8dc1945377481bdc3e2475276a
disco07 <koneenok@outlook.fr>
2021-06-23 23:15:20 +0200

    upload file4

--- src/Controller/CreateMediaObjectAction.php
+++ src/Controller/CreateMediaObjectAction.php
@@ -7,24 +7,9 @@ namespace App\Controller;
 use App\Entity\Images;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Security\Core\Security;
 
 final class CreateMediaObjectAction
 {
-    /**
-     * @var Security
-     */
-    private $security;
-
-    /**
-     * CreateMediaObjectAction constructor.
-     * @param Security $security
-     */
-    public function __construct(Security $security)
-    {
-        $this->security = $security;
-    }
-
 
     public function __invoke(Request $request): Images
     {
